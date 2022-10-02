c0a9c85d0fc064f5ddfc05ffed4a15508c63a4cb
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2022-10-02 13:11:24 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"path/filepath"
+	"regexp"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	nsClient := clientset.CoreV1().Namespaces()
+	nsList, err := nsClient.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("I found %d namespaces \n", len(nsList.Items))
+
+	const excludedNs string = "default|^kube-."
+
+	rx, err := regexp.Compile(excludedNs)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, b := range nsList.Items {
+		currNs := b.ObjectMeta.Name
+
+		fmt.Printf("Current namespace  %s\n ", currNs)
+
+		found := rx.MatchString(currNs)
+		if found {
+			fmt.Println("Skipping...")
+			continue
+		}
+		toBeDeleted := false
+		podsClient := clientset.CoreV1().Pods(currNs)
+
+		podsList, err := podsClient.List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		podsNumber := len(podsList.Items)
+		if podsNumber == 0 {
+			fmt.Println("No pods in this namespace. Should be deleted")
+			toBeDeleted = true
+		} else {
+			fmt.Printf("This namespace has %d pods \n", podsNumber)
+		}
+
+		svcClient := clientset.CoreV1().Services(currNs)
+
+		svcList, err := svcClient.List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		svcNumber := len(svcList.Items)
+		if svcNumber == 0 {
+			fmt.Println("No services in this namespace. Should be deleted")
+			toBeDeleted = true
+		} else {
+			fmt.Printf("This namespace has %d services \n", svcNumber)
+		}
+
+		if toBeDeleted {
+			fmt.Println(currNs, " will be deleted..")
+			deletePolicy := metav1.DeletePropagationForeground
+			if err := nsClient.Delete(context.TODO(), currNs, metav1.DeleteOptions{
+				PropagationPolicy: &deletePolicy,
+			}); err != nil {
+				panic(err)
+			}
+			fmt.Println(currNs, " has beed deleted successfully")
+		}
+	}
+
+}
